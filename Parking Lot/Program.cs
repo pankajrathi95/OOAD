36367c96d5663113f6fecae8c5bde394c52d582f
Pankaj Rathi <pankaj_rathi@outlook.com>
2021-04-12 21:25:26 +0530

    Hotel management system and Parking Lot LLD

---
+++ Parking Lot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
